62caed76e6d5ead3303353ca4ca35af5bc74aa72
Armin Schlegel <armin.schlegel@gmx.de>
2020-09-10 08:23:10 +0200

    Added i3 config

---
+++ i3/keyboardrotate.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Armin Schlegel, armin.schlegel@gmx.de
+
+usage() { (
+	printf "`basename $0` rotates between two defined keyboard layouts on every call of the script\n"
+	printf "Usage: `basename $0` layout1 layout2\n"
+	printf "Where\n"
+	printf "\tlayout1 - primary layout\n"
+	printf "\tlayout2 - initial secondary layout\n"
+	printf "Example:\n"
+	printf "`basename $0` us de - switches between 'us' and 'de'\n"
+)>&2 ; }
+
+
+
+if [ "$#" -ne 2 ]; then
+    echo "Illegal number of parameters. See --help."
+    exit 1
+fi
+
+if [ "$1" == "--help" ]; then
+    usage
+    exit 0
+fi
+
+
+LAYOUTS=("$@")
+
+CURRENT=$(setxkbmap -print | awk -F"+" '/xkb_symbols/ {print $2}')
+
+for layout in "${LAYOUTS[@]}"; do
+  echo "$CURRENT -> $layout"
+  if [ $CURRENT != $layout ]; then
+    setxkbmap $layout
+    echo $layout > /tmp/xkblayout
+    exit 0
+  fi
+done
+
+
+trap "pkill -P $$; exit" SIGINT
+trap "pkill -P $$; exit" SIGTERM
+trap "pkill -P $$; exit" EXIT
